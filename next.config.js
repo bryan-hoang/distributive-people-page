1f99898a2c93eb44a77efa80b46294d279bd9199
Bryan Hoang <bryan@bryanhoang.dev>
2024-04-20 18:55:12 +0000

    feat: add ability to change basePath based on env

--- next.config.js
+++ next.config.js
@@ -1,16 +1,52 @@
+import createDebug from "debug";
+import dotenv from "dotenv";
+import { object, safeParse, string } from "valibot";
+
+const debug = createDebug("config");
+
+dotenv.config();
+dotenv.config({
+	path: ".env.local",
+	override: true,
+});
+
+const EnvSchema = object({
+	BASE_PATH: string(),
+});
+
+debug(`BASE_PATH: ${process.env.BASE_PATH}`);
+
+let basePath = "";
+const result = safeParse(EnvSchema, process.env);
+
+if (result.success) {
+	basePath += result.output.BASE_PATH;
+} else {
+	throw new TypeError("BASH_PATH environment variable is not set");
+}
+
 /**
- * @type {import('next').NextConfig}
+ * The config object that configures Next.js
+ *
+ * @returns {import('next').NextConfig} The config object.
  */
-const nextConfig = {
-	output: "export",
-	reactStrictMode: true,
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
-	typescript: {
-		ignoreBuildErrors: true,
-	},
-	basePath: "/bryanhoang",
+const nextConfig = () => {
+	/**
+	 * @type {import('next').NextConfig}
+	 */
+	const config = {
+		basePath,
+		output: "export",
+		productionBrowserSourceMaps: true,
+		reactStrictMode: true,
+		eslint: {
+			ignoreDuringBuilds: true,
+		},
+		typescript: {
+			ignoreBuildErrors: true,
+		},
+	};
+	return config;
 };
 
 export default nextConfig;
